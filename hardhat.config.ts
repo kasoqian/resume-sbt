f4944cdf0341774fe400545241a9727dfcc0636c
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-07-15 19:13:01 +0800

    chore: 完成基本项目配置

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import privateKey from "./config";
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.15",
+  defaultNetwork: "rinkeby", // "hardhat" or "rinkeby"
+  networks: {
+    hardhat: {
+      chainId:31337,
+    },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde", // 节点的url
+      accounts: privateKey
+    }
+  },
+  solidity: {
+    version: "0.8.15",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
 };
 
 export default config;
